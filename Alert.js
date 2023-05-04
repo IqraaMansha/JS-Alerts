e14222e83d4db16e1ddce1442d9ea908294b5fbe
Iqra Mansha <iqramansha78u@gmail.com>
2023-05-04 12:30:31 +0500

    First Commit

---
+++ Alert.js
@@ -0,0 +1,19 @@
+// #1 ALERTS
+
+console.log("#1 ALERT");
+
+
+alert("Error! Please Enter a valid Password");
+
+alert("Welcome to JS Land \nHappy Coding ");
+
+alert("Hello...I can run JS through My Web Browser’s Console ");
+
+alert("Hello There!");
+
+// alert(5 + 5);
+
+alert("My Name is Iqra");
+
+alert("“__________ஜ۩۞۩ஜ__________”");
+
