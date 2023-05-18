ffe3ade15a35b6fcbba2f1137972ed01b95cdf97
Iqra Mansha <115552409+IqraaMansha@users.noreply.github.com>
2023-05-18 23:51:13 +0500

    Update Alert.js

--- Alert.js
+++ Alert.js
@@ -8,6 +8,7 @@ console.log("#1 ALERT");
 alert("Error! Please Enter a valid Password");
 
 
+
 alert("Welcome to JS Land \nHappy Coding ");
 
 alert("Hello...I can run JS through My Web Browser’s Console ");
